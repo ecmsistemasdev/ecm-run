d5532038fd1bbac456df71f1f150c41d224a5117
ecmsistemasdev <164636970+ecmsistemasdev@users.noreply.github.com>
2024-12-26 12:38:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello World!</h1>"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
     
