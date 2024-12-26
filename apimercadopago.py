54a1e70acfabc0f3770b912282c2fc269579a37e
ecmsistemasdev <164636970+ecmsistemasdev@users.noreply.github.com>
2024-12-26 17:05:35 -0400

    Update apimercadopago.py

--- apimercadopago.py
+++ apimercadopago.py
@@ -1,18 +1,16 @@
 import mercadopago
 
+def gerar_link_pagamento():
 
-
-def gerar_link_pagamento(title, quantity, unit_price):
-    
     sdk = mercadopago.SDK("APP_USR-4419840842819511-121317-8c522deb54aff8ea290465f557bcdf0b-96531112")
 
     payment_data = {
         "items": [
             {"id": "1", 
-             "title": title, 
-             "quantity": quantity, 
+             "title": 'Teste', 
+             "quantity": 1, 
              "currency_id": "BRL", 
-             "unit_price": unit_price}],
+             "unit_price": 1}],
         "back_urls": {
             "success": "http://ecmrun.com.br/sucesso",
             "failure": "http://ecmrun.com.br/falha",
@@ -26,6 +24,6 @@ def gerar_link_pagamento(title, quantity, unit_price):
 
     # Obtém a resposta do pagamento
     payment = result["response"]
-    link_iniciar_pagamento = payment["init_point"]
+    #link_iniciar_pagamento = payment["init_point"]
 
-    return link_iniciar_pagamento
+    return payment #link_iniciar_pagamento
