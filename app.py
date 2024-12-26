f3678e2feccfa96134889589bbeb2d2b508f24ec
ecmsistemasdev <164636970+ecmsistemasdev@users.noreply.github.com>
2024-12-26 14:23:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+port = int(os.environ.get("PORT", 5000))
+
 @app.route("/")
 def index():
-    return "<h1>Hello World!</h1>"
+    return f'<h1>Hello World! </h1>'
 
-#if __name__ == "__main__":
+
+
+#app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
+
 #    app.run(debug=True)
     
